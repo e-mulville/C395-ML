9ee7d03a1acc452a5f0c42131133808eb1d57331
Alessandro Serena <as6316@sprite20.doc.ic.ac.uk>
2019-01-30 11:56:57 +0000

    Add test and prediction functionalities

--- validation.py
+++ validation.py
@@ -1,8 +1,14 @@
+# Import libraries
 import numpy as np
 import math
 import time
+from decisiontree import decision_tree_learning as getTree
 
-def crossValidate(data_set){
+# Define constants
+NUM_CLASSES = 4
+
+
+def crossValidate(data_set):
 
     split_size = data_set.size()/10;
 
@@ -26,21 +32,67 @@ def crossValidate(data_set){
 
 
     #average the test reults
-}
 
-def train_and_prune(validation_set, training_set){
 
+#########################################################
+
+def train_and_prune(validation_set, training_set):
+    None
     #build tree from training_set
 
     #prune it by comparing to validation_set
 
     #return tree for testing
 
-}
+#########################################################
+# Given a tree and a single measurement (7 values)
+# return the predicted class
+def predict(tree, value):
+    # If tree is leaf, return its value
+    if (tree["leaf"] != 0):
+        return int(tree["leaf"])
+    # Else traverse 1 level of the tree depending on the measurement and iterate recursively
+    else:
+        attr = tree["attribute"]
+        split_val = tree["value"]
+        if (value[attr] < split_val):
+            return predict(tree["left"], value)
+        else:
+            return predict(tree["right"], value)
 
 
-def test(tree, test_set){
-    #test the tree
+#########################################################
+# Test tree based on test set and return 5-tuple
+# (Confusion Matrix | Recall | Precision | F score | Classification rate)
+def test(tree, test_set):
+    # Initialize Confusion Matrix
+    CM = np.zeros((NUM_CLASSES, NUM_CLASSES))
+    # Test the tree and fill CM
+    for test_val in test_set:
+        # for each value in the test set, evaluate the prediction
+        classPredicted = predict(tree, test_val)
+        # check whether it matches the label and update confusion matrix accordingly
+        trueLabel = int(test_val[7])
+        CM[trueLabel-1][classPredicted-1] += 1
+    
+    # TODO
+    # # From CM calculate other metrics
+    # for metricID in range (1,5):
+    #     metricTemp = np.zeros(NUM_CLASSES)
+    #     # 1 - Recall
+        
+    #     # 2 - Precision
+    #     # 3 - F score
+    #     # 4 - Classification
+    # Return the metrics
+    
+    print(CM)
 
-    #return the metrics
-}
+
+
+print("Reading datafiles...")
+dataSet = np.loadtxt('co395-cbc-dt/wifi_db/clean_dataset.txt')
+print("Producing tree...")
+tree = getTree(dataSet, 0)[0]
+print("Testing tree...")
+test(tree, dataSet)
