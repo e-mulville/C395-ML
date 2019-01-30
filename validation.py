b3a666ce08e53474935a0572708ad5e993925fb4
alexander3605 <as6316@ic.ac.uk>
2019-01-30 17:06:52 +0000

    add support for all metrics

--- validation.py
+++ validation.py
@@ -2,6 +2,7 @@
 import numpy as np
 import math
 import time
+#from pruning import *
 from decisiontree import decision_tree_learning as getTree
 
 # Define constants
@@ -17,7 +18,7 @@ def crossValidate(data_set):
 
         split_set = np.split(data_set,[i*split_size, (i+1)*split_size])
         test_set = split_set[1]
-        training_set = np.concatinate(split_set[0],split_set[1])
+        training_set = np.concatenate(split_set[0],split_set[1])
 
         #split out the validation
         for j in range(0,9):
@@ -65,11 +66,15 @@ def predict(tree, value):
 
 
 #########################################################
-# Test tree based on test set and return 5-tuple
+# Tests tree based on test set and return 5-tuple
 # (Confusion Matrix | Recall | Precision | F score | Classification rate)
 def test(tree, test_set):
-    # Initialize Confusion Matrix
+    # Initialize Confusion Matrix and other metrics variables
     CM = np.zeros((NUM_CLASSES, NUM_CLASSES))
+    recall = 0
+    precision = 0
+    Fscore = 0
+    classificationRate = 0
     # Test the tree and fill CM
     for test_val in test_set:
         # for each value in the test set, evaluate the prediction
@@ -77,20 +82,42 @@ def test(tree, test_set):
         # check whether it matches the label and update confusion matrix accordingly
         trueLabel = int(test_val[7])
         CM[trueLabel-1][classPredicted-1] += 1
-
-    # TODO
-    # # From CM calculate other metrics
-    # for metricID in range (1,5):
-    #     metricTemp = np.zeros(NUM_CLASSES)
-    #     # 1 - Recall
-
-    #     # 2 - Precision
-    #     # 3 - F score
-    #     # 4 - Classification
+    # From CM calculate other metrics
+        # 1 - Recall
+    recallSum = 0
+    for room in range(NUM_CLASSES):
+        tp = CM[room][room]
+        fn = 0
+        for x in range(NUM_CLASSES):
+            if (x != room): 
+                fn += CM[room][x]
+            recallTemp = tp / (tp + fn)
+        recallSum += recallTemp
+    recall = recallSum / NUM_CLASSES
+        # 2 - Precision
+    precisionSum = 0
+    for room in range(NUM_CLASSES):
+        tp = CM[room][room]
+        fp = 0
+        for x in range(NUM_CLASSES):
+            if (x != room): 
+                fp += CM[x][room]
+        precisionTemp = tp / (tp + fp)
+        precisionSum += precisionTemp
+    precision = precisionSum / NUM_CLASSES
+        # 3 - F score
+    Fscore = 2 * precision * recall / (precision + recall)
+    # 4 - Classification rate
+    correctClass = 0
+    totalData = 0
+    for room in range(NUM_CLASSES):
+        correctClass += CM[room][room]
+        for x in range(NUM_CLASSES):
+            totalData += CM[room][x]
+    classificationRate = correctClass / totalData
+    
     # Return the metrics
-
-    print(CM)
-
+    return (CM, recall, precision, Fscore, classificationRate)
 
 
 print("Reading datafiles...")
@@ -98,4 +125,13 @@ dataSet = np.loadtxt('co395-cbc-dt/wifi_db/clean_dataset.txt')
 print("Producing tree...")
 tree = getTree(dataSet, 0)[0]
 print("Testing tree...")
-test(tree, dataSet)
+# select TESTSET
+testSet = dataSet
+metrics = test(tree, testSet)
+print("Confusion matrix")
+print(metrics[0])
+print("Recall\t", metrics[1])
+print("Precision\t", metrics[2])
+print("F Score\t", metrics[3])
+print("Accuracy\t", metrics[4])
+
