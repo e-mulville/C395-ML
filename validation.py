3302a0affbd77515bebbb8316b6b9ddd034c81c8
e-mulville <endampmulville@gmail.com>
2019-02-06 11:22:54 +0000

    Testing testing function

--- validation.py
+++ validation.py
@@ -28,6 +28,11 @@ def crossValidate(data_set):
             validation_set = split_training_set[1]
             training_set = np.concatenate((split_training_set[0],split_training_set[2]), axis = 0)
 
+            decisiontree = getTree(training_set,0)[0]
+
+            print(decisiontree)
+            print (test(decisiontree,test_set))
+
             # produced_tree  = train_and_prune(validation_set, training_set)
             #
             # test_results[i][j] = test(produced_tree, test_set)
