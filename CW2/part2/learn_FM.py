ce622e8c04aa43c01f46ac21fc04bb470adc8b7d
Enda Mulville <endampmulville@gmail.com>
2019-02-27 23:18:56 +0000

    Hyperparameter searching continued

--- CW2/part2/learn_FM.py
+++ CW2/part2/learn_FM.py
@@ -39,6 +39,23 @@ def main():
     rate = 0.25
 
 
+    def evaluate(network):
+        predictions = network.predict(x_test)
+        predictions.tolist()
+        y_true = y_test.tolist()
+
+        loss = [0.0, 0.0, 0.0]
+
+        for i in range(len(predictions)):
+            for j in range(len(predictions[i])):
+                loss[j] += ((predictions[i][j] - y_true[i][j])**2)
+
+        for i in range(3):
+             loss[i] = loss[i]/len(predictions)
+             loss[i] = loss[i]**0.5
+
+        return ((loss[0]+loss[1]+loss[2])/3)
+
     def first_network():
         network = keras.models.Sequential()
         # Layers
@@ -72,49 +89,72 @@ def main():
         print("Test accuracy:", score[1], "%")
 
         # Get predictions on test data
-        predictions = network.predict(x_test)
-        predictions.tolist()
-        y_true = y_test.tolist()
-
-        loss = [0.0, 0.0, 0.0]
-
-        for i in range(len(predictions)):
-            for j in range(len(predictions[i])):
-                loss[j] += ((predictions[i][j] - y_true[i][j])**2)
-
-        for i in range(3):
-             loss[i] = loss[i]/len(predictions)
-             loss[i] = loss[i]**0.5
-        print("Loss:", loss)
+        print(evaluate(network))
 
     def hyperparameter():
 
         # Layers
+        best_performance = 100.0
+        best_network = keras.models.Sequential()
 
+        for neurons in range(4,8):
+            for layers in range(0,2):
+            #for num_epochs in range (1,7):
+            #for dropout_rate in range(0,4):
 
-        for j in range(1,7):
-            for i in range(0,3):
+                    #INITAL NETWORK
+
                 network = keras.models.Sequential()
 
-                network.add( Dense(j*100, input_shape=(3,), activation="relu", kernel_constraint=max_norm(3) ) )
+                network.add( Dense(neurons*100, input_shape=(3,), activation="relu") )
+                #network.add( Dropout(dropout_rate*0.1))
 
-                 #Layers
-                for x in range(i):
-                    network.add( Dense(j*100, activation="relu", kernel_constraint=max_norm(3) ) )
-
+                 #ADDING MORE LAYERS
+                for x in range(layers):
+                    network.add( Dense(neurons*100, activation="relu", kernel_constraint=max_norm(3) ) )
+                    #network.add( Dropout(dropout_rate*0.1))
                 network.add( Dense(NUM_CLASSES, activation="linear") )
 
                 network.compile(    loss = 'mean_squared_error',
                                     optimizer = "adam",
                                     metrics = ["accuracy"])
-                print(network.summary())
+
+                print(network.summary()) # DEBUG
+
+                #EVALUATION
+
+                print("################ Training ##################")
+
+                batch_size = 50
+                epochs = 1000
+                network.fit(x_train, y_train, batch_size, epochs, verbose=0)
+
+                score = network.evaluate(x_test, y_test, verbose=0)
+                print("Test loss    :", score[0])
+                print("Test accuracy:", score[1], "%")
 
 
+
+                if evaluate(network) < best_performance:
+                    best_performance = evaluate(network)
+                    best_network = network
+                    print("BETTER")
+
+                # print("Best network so far:")
+                # print(best_network.summary())
+
+
+
+
+        network = best_network
+
+        print("Best network is:")
+        print(best_network.summary())
         network_json = network.to_json()
         with open("network.json", "w") as json_file:
             json_file.write(network_json)
         # serialize weights to HDF5
-        model.save_weights("network.h5")
+        network.save_weights("network.h5")
         print("Saved model to disk")
 
     def predict_hidden(dataset):
@@ -126,7 +166,12 @@ def main():
         loaded_network.load_weights("model.h5")
         print("Loaded model from disk")
 
-    first_network()
+        score = loaded_network.evaluate(x_test, y_test, verbose=0)
+        print("Test loss    :", score[0])
+        print("Test accuracy:", score[1], "%")
+
+
+    #first_network()
     hyperparameter()
 
 
